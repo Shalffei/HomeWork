82d1111756d6678bdc4849a357c84feddedabff0
Тимофей Буланкин <starofhd@gmail.com>
2022-02-04 22:21:50 +0200

    test timofeyko

--- Tank Factory/Program.cs
+++ Tank Factory/Program.cs
@@ -7,3 +7,5 @@ Factory factory = new Factory();
 var myModel = creatTank.GetTank("Middle T-34", Chassis.FortyTone, Engine.SixHundredHP, Frame.Middle, Tower.MiddleTower, Weapon.MiddleWeapon, factory);
 myModel = creatTank.GetTank("Middle T-34", Chassis.FortyTone, Engine.ForHundredHP, Frame.Middle, Tower.MiddleTower, Weapon.MiddleWeapon, factory);
 myModel = creatTank.GetTank("Havy Middle T-10", Chassis.SixtyTone, Engine.SixHundredHP, Frame.Heavy, Tower.HavyTower, Weapon.HeavyWeapon, factory);
+
+//Timofey test
